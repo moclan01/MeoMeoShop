97976dd81a1e7e711fcc39e4b9dae2e3160c7e0f
Moc Lan <milepro98@gmail.com>
2025-04-09 20:19:05 +0700

    add project spring boot

---
+++ meo-meo-shop/src/main/java/com/example/meo_meo_shop/MeoMeoShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.meo_meo_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeoMeoShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeoMeoShopApplication.class, args);
+	}
+
+}
---
+++ meo-meo-shop/src/test/java/com/example/meo_meo_shop/MeoMeoShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.meo_meo_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeoMeoShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
